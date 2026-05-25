6b4340c3d2042be1e2a4211d18ac67d1472303fb
Joan <joanitatimbinp@gmail.com>
2026-05-25 14:48:20 +0700

    fix: rm unused sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import SkillSplit from "@/components/SkillSplit";
 import BuildLogs from "@/components/BuildLogs";
-import ModeSwitch from "@/components/ModeSwitch";
-import FutureDirection from "@/components/FutureDirection";
 import UnlockMode from "@/components/UnlockMode";
 import FinalCTA from "@/components/FinalCTA";
-import TechMarquee from "@/components/TechMarquee";
 
 export default function Home() {
   return (
@@ -14,10 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <SkillSplit />
-      <TechMarquee />
       <BuildLogs />
-      <ModeSwitch />
-      <FutureDirection />
       <UnlockMode />
       <FinalCTA />
     </>
